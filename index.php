968bf21840eb03a9992a7442e1e21fd1f61be47d
oipihamed <icidomar15@hotmail.com>
2021-12-06 11:59:30 -0600

    Ultima Act

--- index.php
+++ index.php
@@ -48,25 +48,16 @@
                     </li>
                     <li><a href="contacto.html">Contacto</a></li>
 
-                    <li><input type="text" id="buscador" class="form-control my-2"></input></li>
+                    <li><input type="text" id="buscador" class="form-control my-2" ></input></li>
                     <li><i class="fa fa-search"></i></li>
                     <div id="respuesta"></div>
                 </ul>
             </div>
         </div>
     </nav>
-    <div class="container-fluid">
-        <div class="row ">
-            <div class="container-fluid txt-wh">
-                <!--TOOL TIP-->
-                <span data-toggle="tooltip" data-placement="top" title="ABRIR CHAT DE AYUDA" class="abrir-chat abchat" ><i class="fa fa-question" ></i></span>
-                <!--FIN TOOL-->
-                <!--TOOL TIP-->
-                <span class="ir-arriba"  data-toggle="tooltip" data-placement="top" title="IR ARRIBA"><img src="/LaLaja/images/milk-icon-min-arrow.png" class="icon-milk" alt=""></span>
-                <!--FIN TOOL-->
-                <div class="wrap-iconos">
-                    <!--CHAT BOT-->
-                    <div class="wrapper-chatbot not-visible">
+    <div class="row green-back pd-10">
+         <!--CHAT BOT-->
+         <div class="wrapper-chatbot not-visible">
                         <div class="title">Chat de Ayuda<a class="fa fa-times close-chat"></a></div>
                         <div class="form">
                             <div class="bot-inbox inbox">
@@ -86,17 +77,36 @@
                         </div>
                     </div>
                     <!--FIN CHATBOT-->
-                    <h3>Lacteos La Laja te ofrece</h3>
-                    <p>Distintos productos lácteos de alta calidad elaborados por manos guanajuatenses, en la región
+        <h1 class="title-p">!PRODUCTOS FAVORITOS!</h1>
+        <div class="row" id="row-card-prod">
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row ">
+            <div class="container-fluid txt-wh">
+                <!--TOOL TIP-->
+                <span data-toggle="tooltip" data-placement="top" title="ABRIR CHAT DE AYUDA"
+                    class="abrir-chat abchat"><i class="fa fa-question"></i></span>
+                <!--FIN TOOL-->
+                <!--TOOL TIP-->
+                <span class="ir-arriba" data-toggle="tooltip" data-placement="top" title="IR ARRIBA"><img
+                        src="/LaLaja/images/milk-icon-min-arrow.png" class="icon-milk" alt=""></span>
+                <!--FIN TOOL-->
+                <div class="wrap-iconos">
+                   
+                    <h1 class="title-p">INFORMACION Y RECETAS</h1>
+
+                    <p class="text-justify f-s-20">Lacteos la laja te ofrece distintos productos lácteos de alta calidad
+                        elaborados por manos guanajuatenses, en la región
                         Laja-Bajío.</p>
                 </div><br>
 
                 <div class="row">
-                    <div class="row  icons" >
-                        <div class="media tooltip-box">
+                    <div class="row  icons">
+                        <div class="media tooltip-box" onClick="jascript:verReceta(1)">
                             <span class="tooltip-info">
                                 <i class="fa fa-eye" aria-hidden="true"></i>
-                                 Dar click para ver recetas
+                                Dar click para ver recetas
                             </span>
                             <div class="pull-left">
                                 <i class="icon-medium"><img class="img-i" src="images/queso.png"></i>
@@ -104,125 +114,85 @@
 
                             <div class="media-body">
                                 <h4 class="media-heading">Quesos</h4>
-                                <p class="">Amplia variedad de quesos, así como distintas presentaciones para deleitar
+                                <p class="text-justify f-s-20">Amplia variedad de quesos, así como distintas
+                                    presentaciones para deleitar
                                     tu paldar.</p>
                             </div>
-
-
                         </div>
                         <div class="receta ">
-
-                            <div class="row justify-content-md-center">
-
-                                <div class="col-sm-6 col-md-6 col-lg-6 col-6 txt-alg-left">
-                                    <i class="fa fa-times-circle-o cl-receta" aria-hidden="true"></i>
-                                </div>
-                                <div class="col-sm-12 col-md-12 col-lg-12 col-12 txt-alg-right">
-                                    <i class="icon-medium"><img class="img-r" src="images/vaca-cocinando.png"></i>
-
-                                </div>
-                            </div>
-                            <img src="images/dedos-queso.png" class="img-al" alt="">
-
-                            <div class="row justify-content-md-center wrap-content-recipe">
-                                <div class="col-md-12 recipe">
-                                    <div class="text-receta">
-                                        <h3> DEDOS DE QUESO LA LAJA</h3>
-                                        <p>Esta receta de dedos de queso es ideal para los chiquitines, el queso
-                                            empanizado lleva unas especies italianas que le dan un sabor original,
-                                            pruébalas.</p>
-
-                                    </div>
-                                </div>
-
-
-                                <div class="col-md-6 col-lg-6 wrap-list-ing">
-                                    <h5>INGREDIENTES PARA 4 PERSONAS</h5>
-                                    <div class="ingredientes-l">
-                                        <ul class="list-ing">
-                                            <li>400 gr <a href="paginas/mockupcompra.php?id_p=1"><strong
-                                                        class="n tooltip-box">Queso
-                                                        <span class="tooltip-info">
-                                                            <img src="images/img-1.jpg" class="img-ing-su" alt="">
-                                                        </span>
-                                                    </strong>
-                                                </a>
-                                            </li>
-                                            <li>1 taza <strong>Harina</strong></li>
-                                            <li>2 <strong>Huevos</strong></li>
-                                            <li>2 tazas de <strong>Pan Molido</strong> extra crunch</li>
-                                            <li> 1 cucharada <strong>Perejil finamente picado</strong></li>
-                                            <li>2 tazas <strong>Aceite</strong> </li>
-                                            <li><strong>Sal</strong> al gusto</li>
-                                            <li><strong>Pimienta</strong> al gusto</li>
-                                        </ul>
-
-                                    </div>
-                                </div>
-                                <div class="col-md-6 col-lg-6 wrap-list-step">
-                                    <h4>PASOS:</h4>
-                                    <ol>
-                                        <li>En un recipiente hondo mezcle los huevos y el agua.</li>
-                                        <li>En otro recipiente mezcle el pan molido, la sal de ajo, la albahaca, y el
-                                            orégano.</li>
-                                        <li>En un tercer recipiente hondo mezcle la harina y la maicena.</li>
-                                        <li>Caliente en un sartén grande el aceite hasta que esté listo para freír.</li>
-                                        <li>Corte el queso manchego en rectángulos (los dedos) y pase cada uno por la
-                                            mezcla de la harina, luego la de los huevos y por último la del pan molido.
-                                        </li>
-                                        <li>Fría cada dedo hasta que este dorado (aproximadamente 45 segundos) y seque
-                                            muy bien con una toalla de papel para remover la grasa.
-                                        </li>
-                                    </ol>
-
-
-                                </div>
-
-                            </div>
                         </div>
-                    </div>
+                 
 
-                    <div class="media tooltip-box">
-                    <span class="tooltip-info">
-                                <i class="fa fa-eye" aria-hidden="true"></i>
-                                 Dar click para ver recetas
-                            </span>
+                    <div class="media tooltip-box" onClick="jascript:verReceta(2)">
+                        <span class="tooltip-info">
+                            <i class="fa fa-eye" aria-hidden="true"></i>
+                            Dar click para ver recetas
+                        </span>
                         <div class="pull-left">
                             <i class="icon-medium"><img class="img-i"
                                     src="http://www.lacteoslalaja.com/core/img/crema.png"></i>
                         </div>
                         <div class="media-body">
                             <h4 class="media-heading">Crema</h4>
-                            <p>Contamos con crema natural y media crema, deliciosa y fresca.</p>
+                            <p class="text-justify f-s-20">Contamos con crema natural y media crema, deliciosa y fresca.
+                            </p>
                         </div>
                     </div>
+                    <div class="media tooltip-box" onClick="jascript:verReceta(3)">
+                        <span class="tooltip-info"><i class="fa fa-eye" aria-hidden="true"></i>
+                            Dar click para er recetas</span>
 
-                    <div class="media tooltip-box"><span class="tooltip-info"><i class="fa fa-eye" aria-hidden="true"></i> Dar click para er recetas</span>
-                  
                         <div class="pull-left">
                             <i class="icon-medium"><img class="img-i"
                                     src="http://www.lacteoslalaja.com/core/img/yogh.png"></i>
                         </div>
                         <div class="media-body">
                             <h4 class="media-heading">Yoghurt</h4>
-                            <p>Delicioso yoghurt en diversas presentaciones, ideal para tu familia y tu negocio.</p>
+                            <p class="text-justify f-s-20">Delicioso yoghurt en diversas presentaciones, ideal para tu
+                                familia y tu negocio.</p>
                         </div>
                     </div>
 
+                    </div>
                 </div>
+
+
+
+
+
             </div>
         </div>
     </div>
 
-    <div class="row green-back">
-    <h1 class="title-p">PRODUCTOS MAS GUSTADOS!!!!</h1>
-        <div class="row" id="row-card-prod">
+
+
+    <div class="row green-back pd-10" id="row-card-not">
+        <h1 class="title-p">NOTICIAS</h1>
+        <div class="example-2 card">
+            <div class="wrapper">
+                <div class="header">
+                    <div class="date">
+                        <span class="day">12</span>
+                        <span class="month">Agosto</span>
+                        <span class="year">2021</span>
+                    </div>
+                    <ul class="menu-content">
+                        <li>
+                        </li>
+                    </ul>
+                </div>
+                <div class="data">
+                    <div class="content">
+                        <span class="author">La Laja</span>
+                        <h1 class="title"><a href="#">Unete a nuestro grupo de trabajo</a></h1>
+                        <p class="text">Se parte del grupo de trabajo quesos LaLaja, ofrecemos sueldo competitivo,
+                            capacitacion y beneficios, solo es necesario traer tu documentacion y seguir los pasos en
+                            descritos ...</p>
+                        <a href="#" class="button">Contactanos</a>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-    <div class="row" id="row-card-not">
-        
-    </div>
-    </div>
     </div>
 
     <footer class="footer">
